f24cf37b9af75507e8b8d6594cb0905363bb8168
Vitafresh <vvitafresh@gmail.com>
2019-02-11 21:44:19 +0200

    Forming parameters, changed background pictures

--- app/src/main/java/top/vitafresh/climate/WeatherController.java
+++ app/src/main/java/top/vitafresh/climate/WeatherController.java
@@ -3,13 +3,26 @@ package top.vitafresh.climate;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.loopj.android.http.RequestParams;
+
 public class WeatherController extends AppCompatActivity {
     //TODO: Declare constants
+
+    //URL to retrieve weather data
+    private final String URL_SITE="http://api.openweathermap.org/data/2.5/weather";
+    // App ID to use OpenWeather data
+    private final String APP_ID = "5e8752a808837d9b64aa7990b885a9f9";
+    //Set metric units constatnt
+    private final String MEASURE_UNITS = "metric";
+
+    private String mCity = "Unknown";
+
 
     //TODO: Declare member variables
     private ImageButton btnCityChange;
@@ -36,6 +49,7 @@ public class WeatherController extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent myIntent = new Intent(WeatherController.this,ChangeCityController.class);
+                //Send focus to the Activity with City selection (CahngeCityController.class)
                 startActivity(myIntent);
             }
         });
@@ -48,20 +62,53 @@ public class WeatherController extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        Log.d("Climate","onResume");
+
         Intent myIntent = getIntent();
-        String city = myIntent.getStringExtra("city");
-        textCity.setText(city);
+        //Get data from City selection Activity
+        mCity = myIntent.getStringExtra("city");
+        if(mCity != null) {
+            textCity.setText(mCity);
+            getWeatherForCity(mCity);
+        }
+        else{
+            textCity.setText("Undefined");
+            getWeatherByLocation();
+        }
     }
 
 
 
     //TODO: Add Weather for current location
+    private void getWeatherByLocation(){
+        RequestParams params=new RequestParams();
+        params.add("appid",APP_ID);
+        params.add("lat","latitude");
+        params.add("lon","longitude");
+        params.add("units",MEASURE_UNITS);
+
+
+        getJsonWeatherByApi(params);
+
+    }
 
     //TODO: Add Weather for the city
+    private void getWeatherForCity(String city){
+        RequestParams params=new RequestParams();
+        params.add("appid",APP_ID);
+        params.add("q",city);
+        params.add("units",MEASURE_UNITS);
+        getJsonWeatherByApi(params);
+    }
 
     //TODO: Do API call to website (using prepared RequestParams)
+    private void getJsonWeatherByApi(RequestParams params){
+        Log.d("Climate","params: " + params.toString());
+    }
 
     //TODO: Add UpdateUI (set data for views)
 
     //TODO: Add onPause()
+
+    //TODO: Refresh Weather data by Swipe from top to bottom (show "Refresh" text or icon)
 }
--- app/src/main/java/top/vitafresh/climate/WeatherDataModel.java
+++ app/src/main/java/top/vitafresh/climate/WeatherDataModel.java
@@ -1,4 +1,71 @@
 package top.vitafresh.climate;
 
+import org.json.JSONObject;
+
 public class WeatherDataModel {
+
+    //TODO: Declare member variables
+    private String mCity;
+    private String mTemperature;
+    private String mWeatherCondition;
+    private String mIconName;
+
+
+    public static WeatherDataModel getFromJson(JSONObject json){
+        WeatherDataModel weatherObject = new WeatherDataModel();
+
+        //Just for testing
+        weatherObject.mCity="Kharkiv";
+        weatherObject.mTemperature ="33";
+        weatherObject.mWeatherCondition="Snow";
+        weatherObject.mIconName=getWeatherIconName(501);
+
+        return weatherObject;
+    }
+
+    private static String getWeatherIconName(int condition){
+        if (condition >= 0 && condition < 300) {
+            return "tstorm1";
+        } else if (condition >= 300 && condition < 500) {
+            return "light_rain";
+        } else if (condition >= 500 && condition < 600) {
+            return "shower3";
+        } else if (condition >= 600 && condition <= 700) {
+            return "snow4";
+        } else if (condition >= 701 && condition <= 771) {
+            return "fog";
+        } else if (condition >= 772 && condition < 800) {
+            return "tstorm3";
+        } else if (condition == 800) {
+            return "sunny";
+        } else if (condition >= 801 && condition <= 804) {
+            return "cloudy2";
+        } else if (condition >= 900 && condition <= 902) {
+            return "tstorm3";
+        } else if (condition == 903) {
+            return "snow5";
+        } else if (condition == 904) {
+            return "sunny";
+        } else if (condition >= 905 && condition <= 1000) {
+            return "tstorm3";
+        }
+
+        return "dunno";
+    }
+
+    public String getCity() {
+        return mCity;
+    }
+
+    public String getTemperature() {
+        return mTemperature;
+    }
+
+    public String getWeatherCondition() {
+        return mWeatherCondition;
+    }
+
+    public String getIconName() {
+        return mIconName;
+    }
 }
